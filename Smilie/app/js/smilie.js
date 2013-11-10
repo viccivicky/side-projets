066bc4150b701c60908148c7b496475295e1988e
viccivicky <viccivicky@gmail.com>
2013-11-10 23:13:24 +0000

    added more javascript
    added more javascript

--- Smilie/app/js/smilie.js
+++ Smilie/app/js/smilie.js
@@ -31,3 +31,43 @@ if (($item.length % visible) ! = 0) {
 $items.filter(':first').before(items.slice(-visible).clone().addClss('cloned'));
 $items.filter(':last').after($items.slice(0, visiable).clone().addClass('cloned'));
 $items = $slider.find('> li');  //reselect	
+
+//create the carousel padding on left and right(cloned)
+$item.slice(-visible)
+
+$wrapper.scrollLeft(singleWidth * visible);
+
+function gotopage(page) {
+	var dir = page < currentPage ? -1 : 1,
+	n = Math.abs(currentPage - page),
+	left = singleWidth * dir * visible * n;
+
+
+$wrapper.filter(':not(:animated)').animate({
+	scrollLeft : '+=' + left
+}, 500, function () {
+	if (page === 0) {
+		$wrapper.scrollLeft(singleWidth * visible * pages);
+		page = pages;
+	} else if (page > pages) {
+		$wrapper.scrollLeft(singleWidth * visible);
+		page = 1;
+	}
+	currentPage = Page;
+});
+return false;	
+}
+
+$wrapper.after('<a id="arrow_left">&lt;</a><a id="arrow_right">&gt;</a>');
+
+$('a#arrow_left, this').click(function() {
+	return gotoPage(currentPage - 1);
+});
+
+$('a#arrow_right', this).click(function () {
+	return gotoPage(currentPage + 1)
+});
+
+$(this).bind('goto', function (event, page){
+	gotoPage(page);
+});
