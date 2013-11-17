796e757a0b237f86dfad1c5dea477b1472efb274
viccivicky <viccivicky@gmail.com>
2013-11-17 20:46:11 +0000

    colour transition
    colour transition

--- Smilie/app/js/smilie.js
+++ Smilie/app/js/smilie.js
@@ -70,4 +70,65 @@ $('a#arrow_right', this).click(function () {
 
 $(this).bind('goto', function (event, page){
 	gotoPage(page);
-});
+}); 
+
+//Transition Colour Background
+
+// global variables
+var prevLvlNm = 0;
+
+var currLvlNm = 1;
+
+//Set true when the bg is transitioning, false when not
+
+var transition = false;
+
+//If you want the bg change, set true
+//If set false, it will only transition one colour everytime changeBG() is called
+
+var continuousBG = true;
+
+//Array of three values that define the rgb of the last colour
+
+var currentArray;
+
+var game={
+//The amount of ms before the main game loop is called again
+
+runtime: 70,
+
+//how many levels/colours
+
+numOfLvls:3,
+
+//The RGB values for each "level"
+
+colorStyles: {
+	main: '255, 125,0',
+	level1: '255, 125, 0',
+	level2: '0,255,255',
+	level3: '255,0,255',
+},
+}
+
+function changeBG()
+
+{
+//We're beginning to change BG, so let's set the transition to true
+transition = true;
+
+//if we are at last level, transition from last colour to first to restart
+//else, increment the currLvlNm
+if(currLvlNm===game.numOfLvls){currLvlNm=1;prevLvlNm=game.numOfLvls}
+else{prevLvlNm = currLvlNm;currLvlNm++}
+}
+
+
+
+
+
+
+
+
+
+
