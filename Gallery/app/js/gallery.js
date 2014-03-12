bd5033f379d62ac590311184989314ae7a4c97aa
viccivicky <viccivicky@gmail.com>
2014-03-12 17:19:18 +0800

    thumbnail under large image
    added thumbnail under large image

--- Gallery/app/js/gallery.js
+++ Gallery/app/js/gallery.js
@@ -23,14 +23,6 @@ $(function(){
 	{
 		"src" : "app/img/one_eye.png",
 		"description" : "dot eyes"
-	},
-	{
-		"src" : "app/img/shock.png",
-		"description" : "dot eyes"
-	},
-	{	
-		"src" : "app/img/toung.png",
-		"description" : "dot eyes"
 	}
 	];
 
@@ -38,26 +30,70 @@ $(function(){
 		console.log(item);
 		$('<li>').append($('<img>').attr('src', item.src).attr('width', 450).attr('height', 450)).appendTo('.wrapper ul');
 	});
+	
 
 	$(".wrapper").jCarouselLite({
 		btnNext: ".right",
 		btnPrev: ".left",
 		visible: 1
 	});
+	
+	$(data).each(function(index, item){
+		console.log(item);
+		$('<li>').append($('<img>').attr('src', item.src).attr('width', 150).attr('height', 150)).appendTo('.thumbs ul');
+
+		
+	});
 
 });
 
- $(function() {
-	var thumbnails = $("data");
+	
 
-	thumbnails.bind("click", function () {
+
+/* $(function() {
+	$("#data").JSONView(data);
+}
+
+ var thumbs = function(index, item){
+	console.log(thumbs);
+} 
+
+
+ function thumbnails(element){
+	var largeImg = document.getElementById(element);
+	
+	for(var i = 0; i < data.length; i++) {
+	if(i === data[i].src == largeImg.src ) {
+		if(i === imgArray.length){
+			document.getElementById(element).src = data[0].src;
+			break;
+		}
+		document.getElementById(element).src = data[i+1].src;
+		break;
+	}
+}
+
+
+ var thumbnails = function(index, item) {
+	console.log(thumbnails);
+};
+
+ $(function() {
+	var thumbnails = data.map(function(arrayCell){return arrayCell});
+	
+	var thumbnails = [];
+	for(var i = 0; i < data.length; i++) {
+		thumbnails[i] =(data[i]);
+	};
+
+	 thumbnails.bind("click", function () {
 		wrapper.attr({
 			src: $(this).attr("href")
 		})
 
 		return false;
 
-	}); 
+	}); */
 
 
-});
+ 
