7efe142551e5928a272ae1fdfdebb02f77c1c5d7
viccivicky <viccivicky@gmail.com>
2014-07-22 17:20:01 +0100

    recipe kinda working!
    recipe kinda working!

--- Recipe_new/app/js/recipe-book.js
+++ Recipe_new/app/js/recipe-book.js
@@ -46,117 +46,47 @@ $(function() {
 		
 		for(var i= 0; i < recipes.length; i++) {
 			var recipe = recipes[i];
-			$('.wrapper').append('<div class="recipe">' + recipe.label + '</div>'); //create this like the tab element above - recipe element
-			
-		//reference data here - json file label - json file	
+			$('.wrapper').append('<div class="recipe" data-url="'+recipe.json_file+'">' + recipe.label + '</div>');  	
 		}
 	});
 	
-	$('.wrapper').delegate('.recipe', 'click', function() {
-		// Clicking recipe!
-		//grab json file here. set another one above
-		//once get the file name load in json file
-		/*
-				
-			var getJSON = $.getJSON( "app/data/"  + data_var_name , function(json) {
-			  // console.log( "success" );
-			  // The json file loaded correctly, call our functions here
-			  seasons_data = json
-			  populateTabs(json);
-			  
-			}).fail(function() {
-				// There was an error with the json file. 
-		    	console.log( "error" );
-			});
+	function showRecipe(recipe) {
+		$('.wrapper').html('');
+ 		$('.wrapper').append('<h1 class="single_recipe_title">'+recipe.title+'</h1>');
+ 		$('.wrapper').append('<p class= "serving">'+recipe.serving+'</p>');
+ 		$('.wrapper').append('<h2>Ingredients</h2><ul id="ingredients"></ul>');
+ 		for(var i= 0; i < recipe.ingredients.length; i++) {
+	 		$('#ingredients').append('<li>'+recipe.ingredients[i]+'</li>');
+	 	}
+	 	$('.wrapper').append('<h2>Cooking Instructions</h2><ol id="instructions"></ol>');
+ 		for(var i= 0; i < recipe.instructions.length; i++) {
+	 		$('#instructions').append('<li>'+recipe.instructions[i]+'</li>');	
+	 	}
+	}
+	
+	$('.wrapper').on('click', '.recipe', function(evnt) {
+		var json_file = $(this).attr('data-url');
+ 		var json = $.getJSON("app/data/recipies/"+json_file, function(json){
+	 		showRecipe(json);
+	 		
+ 		});
+ 		
 
-		*/
 	});
 	
 	var seasons_nav = {};
 	
-	//var content_links = $('<div />', {'class': 'links'}).appendTo('.wrapper');
-	//	content_links.html(json.items[i].recipies.label);
-	
-	
-	
-	//for loop within a for loop -  show recipes
-	
-	//happen on a click even
-	
 
-	
-	
-		
 			
 	
 	 function recipe_index(){
-		//click event outside the function
-		
-		
-		
-		/*var recipe_seasons = $(this);
-		
-		var position = recipe_seasons.seasons_data('position')*/
-		
-		
+
 			
 	}
 
-	
-	/* function recipe_index() {
-		$("#seasons_nav").click(function(){
-			$(".wrapper").show(json.items[i]recipies.label)
-		});
-	} */
-	
-		
-
-		
-// 1. loop through the json array (hint: the .length of 'items')
-// 2. for each items in the loop, return 'label'
-// 3. append the 'label' value to an li element
-
-// 4. once you have finished the for loop, append all the li elements to the ul (.seasons)
 
 		
 	});
 
 
 });
-/*
-		
-	}
-	
-
-*/
-
-
-// 2 - A div for your season nav tabs 
-
-
-// 3 - A div  for the individual recipies of a selected season
-
-	
-	
-	
-/*
-
-	4 - Load in the first json file with an ajax request
-	file : data/seasons.json	
-*/
-
-
-	
-
-/// when the ajax request is complete: 
-// 5 - Make the data response a global variable called seasons_object so all parts of the app can access it
-
-// 6 - display each season as a tab and assign a data variable called position. This is so you can access the data in the object
-
-
-/* 
-	7 - A function that can be used to set the contents of the container. 
-	 	*  This will display recipies from the seasons_object by using the data position method
-	 	- Create a list of the recipies in the selected season
-	 	- Each item will need a data object called "json_file" on the div
-*/
